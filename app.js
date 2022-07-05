462d4b22792b078a981b694da4e62e35591c002b
Fredrickmakori.git <fredrickmakori102@gmail.com>
2022-07-05 20:21:41 +0300

    I have pulled the changes and also added webkit-cli

---
+++ app.js
@@ -0,0 +1,6 @@
+function changeContent(){
+    var text="Welcome to megaland finance limited, Megalend is an organisation that offers loans woldwide that can defer from one currency to another";
+    var text_1="Megalend conpany is able to offer loans to various persons all over the world no matter which currrency you are using.";
+
+    document.getElementById("introduction").innerHTML=text + "<br>" +text_1
+}
